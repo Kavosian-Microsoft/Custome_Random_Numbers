79ed0aa44960a782f3d3b8ea994be56978570c25
Yashar kavosian <kavoosian@hotmail.com>
2020-03-14 20:23:35 +0330

    I have implemented the project

--- Random_Number_Generator/Random_Number_Generator/Program.cs
+++ Random_Number_Generator/Random_Number_Generator/Program.cs
@@ -14,6 +14,26 @@ namespace Random_Number_Generator
     {
         static void Main(string[] args)
         {
+            Console.Title = "Random Number Generator (RNG)";
+            try
+            {
+                int intStart, intEnd;
+                Console.Write("\n\tEnter start number of the the range:");
+                intStart = int.Parse(Console.ReadLine());
+                Console.Write("\n\tEnter Last number of the the range:");
+                intEnd= int.Parse(Console.ReadLine());
+                Random rnd = new Random(DateTime.Now.Second);
+                Console.Clear();
+                Console.Write("\n\tRange [{0},{1}) : ===> {2}",intStart,intEnd,rnd.Next(intStart,intEnd)); ;
+                
+            }//try
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write("\n\tAn exception has occured");                
+            }//catch
+            Console.Write("\n\tPlease press any key to exit...");
+            Console.ReadKey();
         }//Main
     }//Program
 }//Random_Number_Generator
